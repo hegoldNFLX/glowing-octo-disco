7fd0b1e4359ca5bd011db9cc65df400625ff5b97
Henry Golding <hgolding@netflix.com>
2024-10-01 16:35:03 -0700

    Create blank project

---
+++ repro/Source/repro/repro.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "repro.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, repro, "repro" );
---
+++ repro/Source/repro/reproGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "reproGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REPRO_API AreproGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
